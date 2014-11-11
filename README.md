ba4451991e8719d9f977fc0350341249d33de010
André Alves <andrenpalves@gmail.com>
2014-11-11 23:24:46 +0000

    Javascript changes
    - Easter Egg
    - Active menu item on page scroll
    - Animated scroll changes

--- README.md
+++ README.md
@@ -1,34 +1,19 @@
-Personal
+Personal Website & Portefolio
 ========
 
-Personal Website & Portefolio
+The main purpose of this project is to show a little bit of myself and my work as a software developer.
+It was built using great tools like Sublime Text 2 and Prepos.
+It also uses great libraries: jQuery, Modernizr.js and Normalize.js.
+Fonts are served by the awesomeness of Google Fonts. I am using Montserrat and Neutron.
 
-Used Tools
+
+Testing (TODO)
 ===
-Sublime
-Prepos
+*Browsers
+*Javascript disabled
+*CSS disabled
 
-Used Libraries
+
+Compatibility (TODO)
 ===
-Modernizr
-Normalize
-jQuery
-
-Used Fonts
-===
-Montserrat
-Neutron
-
-Testing
-===
-(TODO)
-Browsers
-Javascript disabled
-CSS disabled
-
-
-Compatibility
-===
-(TODO)
-
-TODO: FAVICON!!!!
+This website is supported by the main browsers.
--- js/script.js
+++ js/script.js
@@ -1,19 +1,29 @@
-$('a[href*=#]:not([href=#])').click(function() {
+//Easter Egg
+console.log(""+
+"                     _       __            _                 \n"+
+"     /\\             | |     /_/      /\\   | |                \n"+
+"    /  \\   _ __   __| |_ __ ___     /  \\  | |_   _____  ___  \n"+
+"   / /\\ \\ | '_ \\ / _` | '__/ _ \\   / /\\ \\ | \\ \\ / / _ \\/ __| \n"+
+"  / ____ \\| | | | (_| | | |  __/  / ____ \\| |\\ V /  __/\\__ \\ \n"+
+" /_/    \\_\\_| |_|\\__,_|_|  \\___| /_/    \\_\\_| \\_/ \\___||___/ \n"+
+"                                                             "+"\n"+
+"                                                             "+"\n"+
+"Hello!\n\n"+
+"This page was built using great tools like Sublime Text 2 and Prepos.\n"+
+"It also uses great libraries such as jQuery, Modernizr and Normalize.js.\n"+
+"Fonts are served by the awesomeness of Google Fonts. I am using Montserrat and Neutron here.\n\n"+
+"Check this project and others at https://github.com/anpa.\n\n---\n");
 
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
-        || location.hostname == this.hostname) {
-
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-           if (target.length) {
-             $('html,body').animate({
-                 scrollTop: target.offset().top
-            }, 1000);
-            return false;
-        }
-    }
+//Animated scroll
+$('a[href*=#]:not([href=#])').click(function(e) {
+  var target = $(this).attr("href");
+  $('html,body').animate({
+    scrollTop: $(target).offset().top
+  }, 1000);
+  e.preventDefault();
 });
 
+//Open/Close menu
 $('#toggle-nav').click(function() {
     if($('header nav').hasClass('nav-closed')) {
         $('header nav').removeClass('nav-closed').addClass('nav-open');
@@ -25,15 +35,23 @@ $('#toggle-nav').click(function() {
     }
 });
 
-/*
-$("nav li a").click(function() {
-    if($('header nav').hasClass('nav-open')) {
-        $('header nav').removeClass('nav-open');
-        $('header nav').addClass('nav-closed');
-    }
-});*/
 
-$(window).scroll(function() {
+//Active menu item on page scroll and header fade
+$(window).scroll(function()  {
+  
+  var treshold = 150;
+  var currentPosition = $(this).scrollTop() + $("header").height() + treshold;
+  
+  var currentSection = $("section").map(function(){
+    if ($(this).offset().top < currentPosition)
+      return this;
+    });
+
+  var sectionId = currentSection[currentSection.length-1].id;
+
+  $("nav li.active").removeClass("active");
+  $("nav li a[href=#" + sectionId + "]").parent().addClass("active");
+
   if ($("header").offset().top > 100) {
     if(!$("header").hasClass("solid")) $("header").addClass("solid");
   } else {
